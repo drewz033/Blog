fb60aed38cc321bb3a1d88314febed4c554cc602
Drew <zagarria@gmail.com>
2020-02-19 20:50:39 -0500

    second commit

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+#model is a class which represents a table in a databases
+
+class Article(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add = True)
+    #add in thumbnail
+    #add in autor
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50]+'...'
