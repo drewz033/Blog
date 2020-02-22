d5ed00033824186b6a75ab566c47b05ba289a4f6
Drew <zagarria@gmail.com>
2020-02-22 14:10:18 -0500

    user login update

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,re_path
+
+from . import views
+
+from django.contrib import admin
+from django.urls import path,re_path
+from . import views
+app_name='accounts'
+
+urlpatterns = [
+    path('signup/', views.signup_view,name='signup'),
+    path('login/',views.login_view,name='login')
+]
